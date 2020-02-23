be99670430670ab54c8f121ceeacdfc218673e44
U-AMDX34GB-PC\papa <muh_kosta@mail.ru>
2020-02-23 15:12:46 +0300

    fix-d_SB_meth_test

--- wolVm_Netbeans/tests/SB.cpp
+++ wolVm_Netbeans/tests/SB.cpp
@@ -1,8 +1,59 @@
-#include <stdlib.h>
-#include <iostream>
-#include "utilMacr.h"
+#include "SB.h"
 #include <string.h>
 #define buf_for_itlwm_keyword 10
+
+StringBuilder::StringBuilder() {
+	ind = 0;
+}
+
+v StringBuilder::Append(l char_) {
+	buff[ind] = char_;
+	ind++;
+}
+
+l * StringBuilder::ToString() {
+	return buff;
+}
+
+v StringBuilder::Clear() {
+	ind = 0;
+}
+
+v StringBuilder::Terminate() {
+	buff[ind + 1] = '\0';
+}
+
+l* StringBuilder::RemoveFirst() {
+	strcpy(buff_n, (char*) (&this->ToString()[1]));
+	strcpy(buff, buff_n);
+	return buff_n;
+}
+
+I StringBuilder::Len() {
+	return strlen(this->buff);
+}
+
+l* StringBuilder::Trim() {// удаляет пробелы | delete whitespaces
+	I ws_cn_before = 0;
+	I le_cn = 0;
+	I count_where_beg_ri_ws = 0;
+	I pos = 0;
+	for (int i = 0; i<this->Len(); i++)
+		if (buff[i] == ' ')
+			ws_cn_before += 1;
+		else
+			break;
+	for (int i = 0; i<this->Len(); i++)
+		if (buff[i] != ' ')
+			le_cn++;
+	count_where_beg_ri_ws = ws_cn_before + le_cn;
+	for (int i = ws_cn_before; i < count_where_beg_ri_ws; i++)
+		buff_n[pos] = buff[i],
+		pos++;
+	//--------------------------------------------------------------------------
+	return buff_n;
+}
+
 
 
 
